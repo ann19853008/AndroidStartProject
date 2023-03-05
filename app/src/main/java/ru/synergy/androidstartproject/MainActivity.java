b4138c69435a5b079b45a274b779eb80eec68f28
ann19853008 <ann19853008@mail.ru>
2023-03-05 14:51:06 +0300

    Added java Script configuration example this constraintLayout

--- app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
@@ -1,6 +1,8 @@
 package ru.synergy.androidstartproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Constraints;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -12,12 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
        // setContentView( R.layout.activity_main );
 
+        ConstraintLayout constraintLayout=new ConstraintLayout( this );
+
         TextView  textView = new TextView(this);
 
         textView.setText("Hello Android!");
 
-        textView.setTextSize(22);
+        textView.setTextSize(30);
 
-        setContentView( textView );
+        ConstraintLayout.LayoutParams layoutParams= new Constraints.LayoutParams( ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT );
+        layoutParams.leftToLeft=ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop =ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.rightToRight=ConstraintLayout.LayoutParams.PARENT_ID;
+        textView.setLayoutParams( layoutParams );
+        constraintLayout.addView(textView  );
+
+        setContentView( constraintLayout);
     }
 }
